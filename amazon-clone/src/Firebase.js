8858af9ea845114bdf10b95a09b291aaeb6bc448
cjriggs27 <cjriggs27@gmail.com>
2020-09-10 11:19:00 -0400

    Starting amazon clone. Styling header

---
+++ amazon-clone/src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYq7RGkJGHaIQgzLvBdvOZg1_KlRCiAKQ",
+    authDomain: "clone-12157.firebaseapp.com",
+    databaseURL: "https://clone-12157.firebaseio.com",
+    projectId: "clone-12157",
+    storageBucket: "clone-12157.appspot.com",
+    messagingSenderId: "365444640439",
+    appId: "1:365444640439:web:ffb469273890c9acb92732",
+    measurementId: "G-4TFW0JZ6ZC"
+  };

59c32aa874e305734c7b277c2206a98483018ab3
cjriggs27 <cjriggs27@gmail.com>
2020-09-11 14:10:48 -0400

    set up react context api

--- amazon-clone/src/index.js
+++ amazon-clone/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./Reducer";
 
 ReactDOM.render(
   <React.StrictMode>

6641bdc5fb224c0d7dd4c24d6ddc151e5621b546
cjriggs27 <cjriggs27@gmail.com>
2020-09-14 12:42:10 -0400

    create users with firebase auth and email/password sign-in provider

--- amazon-clone/src/Firebase.js
+++ amazon-clone/src/Firebase.js
@@ -1,10 +1,19 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDYq7RGkJGHaIQgzLvBdvOZg1_KlRCiAKQ",
-    authDomain: "clone-12157.firebaseapp.com",
-    databaseURL: "https://clone-12157.firebaseio.com",
-    projectId: "clone-12157",
-    storageBucket: "clone-12157.appspot.com",
-    messagingSenderId: "365444640439",
-    appId: "1:365444640439:web:ffb469273890c9acb92732",
-    measurementId: "G-4TFW0JZ6ZC"
-  };
+  apiKey: "AIzaSyDYq7RGkJGHaIQgzLvBdvOZg1_KlRCiAKQ",
+  authDomain: "clone-12157.firebaseapp.com",
+  databaseURL: "https://clone-12157.firebaseio.com",
+  projectId: "clone-12157",
+  storageBucket: "clone-12157.appspot.com",
+  messagingSenderId: "365444640439",
+  appId: "1:365444640439:web:ffb469273890c9acb92732",
+  measurementId: "G-4TFW0JZ6ZC",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };

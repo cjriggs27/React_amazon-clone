3f0b982d5fd7067f8476dc8fcb755c36c6e7dc84
cjriggs27 <cjriggs27@gmail.com>
2020-09-14 20:14:37 -0400

    adding payment page and structuring layout

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -29,7 +29,6 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-
         </div>
       </div>
 
---
+++ amazon-clone/src/Payment.js
@@ -0,0 +1,49 @@
+import React from "react";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        {/* payment section - delivery address */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+            <p>123 Fake Lane</p>
+            <p>Washington, DC</p>
+          </div>
+        </div>
+
+        {/* payment section - Review Items */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className='payment__item'>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* payment section - Payment Method */}
+        <div className='payment__section'></div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
